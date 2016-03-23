9439a195cad0609a71459fc05c676799da1c6521
Sacha David <Sach1810@Sachas-MacBook-Pro.local>
2016-03-23 16:35:04 +0200

    closures added for Aaron

--- script.js
+++ script.js
@@ -1,53 +1,112 @@
-var reviews = [{beerName: 1, category: "Porter", rating: 3}, {beerName: 2, category: "Porter", rating: 5},{beerName: 3, category: "Porter", rating: 1}];
+var BeerReviewApp = function(){
+  var reviews = [{beerName: 1, category: "Porter", rating: 3}, 
+                {beerName: 2, category: "Porter", rating: 5},
+                {beerName: 3, category: "Porter", rating: 1}];
 
-var checker = true;
+    var createBeer = function(beerName, category, rating){
+        var newBeer = {
+          beerName: beerName,
+          category: category,
+          rating: rating
+          }
+        
+        reviews.push(newBeer);
 
-var addBeer = function addBeer(name, category) {
-
-  var beerName = $('#beer-name').val();
-  var category = $('#beer-category').val();
-  var rating = $('#rating-category').val();
-  $('.beers').append("<p> Beer Name: " + beerName + " Category: " + category + "rating: " + rating + "</p><br>");
-
-    var newBeer = {
-    beerName: beerName,
-    category: category,
-    rating: rating
     }
-    reviews.push(newBeer);
-    updateBeers();
-};
+    var renderBeer = function(beerName, category, rating){
+          $('.beers').append("<p> Beer Name: " + beerName + " Category: " + category + " rating: " + rating + "</p><br>");
 
-var updateBeers = function updateBeers(){
-  $('.beers').find('p').remove();
-  for (var i = 0; i < reviews.length; i++) {
-    $('.beers').append("<p>Beer: " + reviews[i].beerName + " " + reviews[i].category + " " + reviews[i].rating+ "</p>");
-  };
-  
-};
 
-updateBeers();
+    }
+
+
+    return {
+      create: createBeer,
+      render: renderBeer 
+     }
+}
 
 $('.post-beer').on('click', function (e) {
   e.preventDefault();
-  addBeer();
+  var beerName = $('#beer-name').val();
+  var category = $('#beer-category').val();
+  var rating = $('#rating-category').val();
 
+  var beerReviewApp = BeerReviewApp();
+  
+  beerReviewApp.create(beerName, category,rating);
+  beerReviewApp.render(beerName, category,rating);
+
+
+
+  BeerReviewApp(beerName, category, rating);
 });
 
-$('.sort-beer').on('click', function (e) {
-  e.preventDefault();
-    if (checker) {
-    reviews.sort(function(obj1, obj2) {
-     return obj2.rating - obj1.rating;
-}); 
-    checker = false;
-  } else {
-    reviews.sort(function(obj1, obj2) {
-     return obj1.rating - obj2.rating;
+
+// var beerName = $('#beer-name').val();
+//   var category = $('#beer-category').val();
+//   var rating = $('#rating-category').val();
+//   $('.beers').append("<p> Beer Name: " + beerName + " Category: " + category + "rating: " + rating + "</p><br>");
+
+
+
+
+
+
+
+
+// var reviews = [{beerName: 1, category: "Porter", rating: 3}, 
+//               {beerName: 2, category: "Porter", rating: 5},
+//               {beerName: 3, category: "Porter", rating: 1}];
+
+// var checker = true;
+
+// var addBeer = function addBeer(name, category) {
+
+  // var beerName = $('#beer-name').val();
+  // var category = $('#beer-category').val();
+  // var rating = $('#rating-category').val();
+  // $('.beers').append("<p> Beer Name: " + beerName + " Category: " + category + "rating: " + rating + "</p><br>");
+
+//     var newBeer = {
+//     beerName: beerName,
+//     category: category,
+//     rating: rating
+//     }
+//     reviews.push(newBeer);
+//     updateBeers();
+// };
+
+// var updateBeers = function updateBeers(){
+//   $('.beers').find('p').remove();
+//   for (var i = 0; i < reviews.length; i++) {
+//     $('.beers').append("<p>Beer: " + reviews[i].beerName + " " + reviews[i].category + " " + reviews[i].rating+ "</p>");
+//   };
+  
+// };
+
+// updateBeers();
+
+// $('.post-beer').on('click', function (e) {
+//   e.preventDefault();
+//   addBeer();
+
+// });
+
+// $('.sort-beer').on('click', function (e) {
+//   e.preventDefault();
+//     if (checker) {
+//     reviews.sort(function(obj1, obj2) {
+//      return obj2.rating - obj1.rating;
+// }); 
+//     checker = false;
+//   } else {
+//     reviews.sort(function(obj1, obj2) {
+//      return obj1.rating - obj2.rating;
     
-  });
-     checker = true;
+//   });
+//      checker = true;
      
-      };
-      updateBeers();
-});
+//       };
+//       updateBeers();
+// });
